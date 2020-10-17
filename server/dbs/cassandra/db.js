e4c4179682cc5c6368d2c3348f92bab453b44250
Chris Bell <chrisbell.dev@gmail.com>
2020-10-17 00:15:14 -0700

    Begin implementing cql seeding script

---
+++ server/dbs/cassandra/db.js
@@ -0,0 +1,11 @@
+const cassandra = require('cassandra-driver');
+
+const client = new cassandra.Client({
+  contactPoints: ['127.0.0.1'],
+  localDataCenter: 'datacenter1',
+  keyspace: 'gallery',
+});
+
+module.exports = {
+  client,
+};
