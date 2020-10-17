3fed9e24aeb45e87db8668f14fbacc1d222dd124
Chris Bell <chrisbell.dev@gmail.com>
2020-10-17 00:18:54 -0700

    Archive previous database

---
+++ server/dbs/mongo/db.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const photo = require('./photoSchema.js');
+
+mongoose.connect('mongodb://localhost:27017/photo', { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose default connection open to: 3001');
+});
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDb database connection established successfully!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log(`Mongoose default connection error: ${err}`);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose default connection disconnected');
+});
+
+// const schema = mongoose.Schema({
+//   userName: 'string',
+// });
+
+// const Repo = mongoose.model('Repo', repoSchema);
+
+module.exports = {
+  photo: photo,
+};
