a27d47c0d459ba253823808c20793e630330f6ef
Chris Bell <chrisbell.dev@gmail.com>
2020-10-17 10:35:12 -0700

    Minor schema adjustments

--- server/dbs/postgres/photo-gallery-schema.sql
+++ server/dbs/postgres/photo-gallery-schema.sql
@@ -13,9 +13,6 @@ CREATE TABLE "restaurants" (
   "site_url" varchar(255),
   "phone_number" varchar(11),
 
-  -- gallery info
-  "gallery_id" int UNIQUE,
-
   -- location info
   "street" varchar(100),
   "city" varchar(100) NOT NULL,
@@ -27,8 +24,8 @@ CREATE TABLE "restaurants" (
 -- users who uploaded photos
 CREATE TABLE "users" (
   "id" SERIAL PRIMARY KEY,
-  "user_url" varchar(255) NOT NULL, -- url of user's profile
-  "user_name" varchar(100) NOT NULL, -- user's full name
+  "user_url" varchar(255) NOT NULL, -- url of user's profile (potentially scale this down)
+  "user_name" varchar(100) NOT NULL, -- user's full name (scale this down)
   "user_review_count" smallint DEFAULT 0, -- number of user's reviews
   "user_friend_count" smallint DEFAULT 0, -- number of user's friends
   "user_photo_count" smallint DEFAULT 0, -- number of user's photos
@@ -38,11 +35,11 @@ CREATE TABLE "users" (
 -- photos table grouped by gallery id
 CREATE TABLE "photos" (
   "id" SERIAL PRIMARY KEY,
-  "gallery_id" int REFERENCES restaurants(gallery_id) NOT NULL,
+  "restaurant_id" int REFERENCES restaurants(id) NOT NULL,
   "user_id" int REFERENCES users(id) UNIQUE NOT NULL,
   "helpfulCount" int DEFAULT 0, -- helpful rating of photo
   "notHelpfulCount" int DEFAULT 0, -- not helpful rating of photo
-  "photo_url" varchar(255) UNIQUE NOT NULL,
+  "photo_url" varchar(255) UNIQUE NOT NULL, -- (potentially scale this down)
   "caption" text,
   "upload_date" timestamp DEFAULT (now())
 );
