8c0fcb86a4a9d1d879471b71ed8694a7075749e8
Chris Bell <chrisbell.dev@gmail.com>
2020-10-19 22:23:51 -0700

    Add tests for Postgres insertion execution time

--- server/dbs/postgres/db.js
+++ server/dbs/postgres/db.js
@@ -37,8 +37,30 @@ const insertRestaurant = async (document, cb) => {
   }
 }
 
+const insertUser = async (document, cb) => {
+  const { user_url, user_name, user_review_count, user_friend_count, user_photo_count, user_elite_status, user_profile_image } = document;
+  try {
+    const res = await client.query('INSERT into users(user_url, user_name, user_review_count, user_friend_count, user_photo_count, user_elite_status, user_profile_image) VALUES($1, $2, $3, $4, $5, $6, $7)', [user_url, user_name, user_review_count, user_friend_count, user_photo_count, user_elite_status, user_profile_image]);
+    cb(null, res);
+  } catch (err) {
+    cb(err, null);
+  }
+}
+
+const insertPhoto = async (document, cb) => {
+  const { restaurant_id, user_id, helpful_count, not_helpful_count, photo_url, caption, upload_date } = document;
+  try {
+    const res = await client.query('INSERT into photos(restaurant_id, user_id, helpful_count, not_helpful_count, photo_url, caption, upload_date) VALUES($1, $2, $3, $4, $5, $6, $7)', [restaurant_id, user_id, helpful_count, not_helpful_count, photo_url, caption, upload_date]);
+    cb(null, res);
+  } catch (err) {
+    cb(err, null);
+  }
+}
+
 module.exports = {
   connectToPool,
   releasePool,
   insertRestaurant,
+  insertUser,
+  insertPhoto,
 };
---
+++ server/dbs/postgres/executions.js
@@ -0,0 +1,62 @@
+const db = require('./db.js');
+const perf = require('execution-time')();
+const fs = require('fs');
+const path = require('path');
+
+const testAll = async () => {
+  let timingResults = [];
+  await db.connectToPool();
+  const restaurantDoc = {
+    restaurant_name: 'Farmhouse Kitchen Thai Cuisine',
+    site_url: 'https://farmhousethai.com/',
+    phone_number: '415-814-2920',
+    city: 'San Francisco',
+    street: '710 Florida St.',
+    state_or_province: 'CA',
+    country: 'US',
+    zip: '94110',
+  };
+  const userDoc = {
+    user_url: 'http://sizabl.io/users/diao',
+    user_name: 'diao',
+    uesr_review_count: 1000,
+    user_friend_count: 1000,
+    user_photo_count: 99,
+    user_elite_status: true,
+    user_profile_image: 'https://i.imgur.com/fPmznir.jpg',
+  };
+  const photoDoc = {
+    restaurant_id: 1,
+    user_id: 1,
+    helpful_count: 100,
+    not_helpful_count: 100,
+    photo_url: 'https://www.cliseetiquette.com/wp-content/uploads/2019/09/waiter-serving-food.jpg',
+    caption: 'Fine dining',
+    upload_date: Date.now(),
+  };
+  perf.start('insertRestaurant');
+  await db.insertRestaurant(restaurantDoc, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    timingResults.push(perf.stop('insertRestaurant'));
+  });
+  perf.start('insertUser');
+  await db.insertUser(userDoc, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    timingResults.push(perf.stop('insertUser'));
+  });
+  perf.start('insertPhoto');
+  await db.insertPhoto(photoDoc, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    timingResults.push(perf.stop('insertPhoto'));
+  });
+  await db.releasePool();
+  fs.writeFileSync(path.join(__dirname, 'generated', 'time.txt'), JSON.stringify(timingResults));
+}
+
+testAll();
