ed610997fb68086d9569eda7cab68256a124163a
Chris Bell <cb.camellia@gmail.com>
2021-01-23 10:05:38 -0800

    Adjust README

--- server/dbs/postgres-db/index.js
+++ server/dbs/postgres-db/index.js
@@ -1,13 +1,13 @@
-const { Pool } = require('pg');
-const user = require('./credentials.js');
-const { query } = require('express');
+const { Pool } = require("pg");
+const user = require("./credentials.js");
+const { query } = require("express");
 
 const pool = new Pool(user.credentials);
 
 // the pool will emit an error on behalf of any idle clients
 // it contains if a backend error or network partition happens
-pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err);
+pool.on("error", (err, client) => {
+  console.error("Unexpected error on idle client", err);
   process.exit(-1);
 });
 
@@ -17,7 +17,7 @@ let client = null;
 const connectToClient = async () => {
   client = await pool.connect();
   // const res = await pool.query('SET search_path to public');
-  console.log('Checked out a client!');
+  console.log("Checked out a client!");
   // set the default schema
 };
 
@@ -25,108 +25,175 @@ const releaseClient = () => {
   // Make sure to release the client before any error handling,
   // just in case the error handling itself throws an error.
   client.release(true);
-  console.log('Released the client');
+  console.log("Released the client");
 };
 
 const insertRestaurant = async (document, analyze, cb) => {
-  const { restaurant_name, site_url, phone_number, city, street, state_or_province, country, zip } = document;
-  const query = `${analyze ? 'EXPLAIN ANALYZE ' : ''}INSERT into restaurants(restaurant_name, site_url, phone_number, city, street, state_or_province, country, zip) VALUES($1, $2, $3, $4, $5, $6, $7, $8)`;
+  const {
+    restaurant_name,
+    site_url,
+    phone_number,
+    city,
+    street,
+    state_or_province,
+    country,
+    zip,
+  } = document;
+  const query = `${
+    analyze ? "EXPLAIN ANALYZE " : ""
+  }INSERT into restaurants(restaurant_name, site_url, phone_number, city, street, state_or_province, country, zip) VALUES($1, $2, $3, $4, $5, $6, $7, $8)`;
   try {
-    const res = await pool.query(query, [restaurant_name, site_url, phone_number, city, street, state_or_province, country, zip]);
+    const res = await pool.query(query, [
+      restaurant_name,
+      site_url,
+      phone_number,
+      city,
+      street,
+      state_or_province,
+      country,
+      zip,
+    ]);
     cb(null, res);
   } catch (err) {
     client.release();
     cb(err, null);
   }
-}
+};
 
 const selectRestaurantByID = async (restaurant_id, analyze, cb) => {
-  const query = `${analyze ? 'EXPLAIN ANALYZE ' : ''}SELECT * from restaurants WHERE restaurant_id = $1`;
+  const query = `${
+    analyze ? "EXPLAIN ANALYZE " : ""
+  }SELECT * from restaurants WHERE restaurant_id = $1`;
   try {
     const res = await pool.query(query, [restaurant_id]);
     cb(null, res);
   } catch (err) {
     cb(err, null);
   }
+};
 
-}
-
-const deleteRestaurantByID = async(restaurant_id, analyze, cb) => {
-  const query = `${analyze ? 'EXPLAIN ANALYZE ' : ''}DELETE from restaurants WHERE restaurant_id = $1`;
+const deleteRestaurantByID = async (restaurant_id, analyze, cb) => {
+  const query = `${
+    analyze ? "EXPLAIN ANALYZE " : ""
+  }DELETE from restaurants WHERE restaurant_id = $1`;
   try {
     const res = await pool.query(query, [restaurant_id]);
     cb(null, res);
   } catch (err) {
     cb(err, null);
   }
-}
+};
 
 const insertUser = async (document, analyze, cb) => {
-  const { user_url, user_name, user_review_count, user_friend_count, user_photo_count, user_elite_status, user_profile_image } = document;
-  const query = `${analyze ? 'EXPLAIN ANALYZE ' : ''}INSERT into users(user_url, user_name, user_review_count, user_friend_count, user_photo_count, user_elite_status, user_profile_image) VALUES($1, $2, $3, $4, $5, $6, $7)`;
+  const {
+    user_url,
+    user_name,
+    user_review_count,
+    user_friend_count,
+    user_photo_count,
+    user_elite_status,
+    user_profile_image,
+  } = document;
+  const query = `${
+    analyze ? "EXPLAIN ANALYZE " : ""
+  }INSERT into users(user_url, user_name, user_review_count, user_friend_count, user_photo_count, user_elite_status, user_profile_image) VALUES($1, $2, $3, $4, $5, $6, $7)`;
   try {
-    const res = await pool.query(query, [user_url, user_name, user_review_count, user_friend_count, user_photo_count, user_elite_status, user_profile_image]);
+    const res = await pool.query(query, [
+      user_url,
+      user_name,
+      user_review_count,
+      user_friend_count,
+      user_photo_count,
+      user_elite_status,
+      user_profile_image,
+    ]);
     cb(null, res);
   } catch (err) {
     cb(err, null);
   }
-}
+};
 
-// select users by id
+// select user by id
 const selectUserByID = async (user_id, analyze, cb) => {
-  const query = `${analyze ? 'EXPLAIN ANALYZE ' : ''}SELECT * from users WHERE user_id = $1`;
+  const query = `${
+    analyze ? "EXPLAIN ANALYZE " : ""
+  }SELECT * from users WHERE user_id = $1`;
   try {
     const res = await pool.query(query, [user_id]);
     cb(null, res);
   } catch (err) {
     cb(err, null);
   }
-}
+};
 
 // delete users by id
-const deleteUserByID = async(user_id, analyze, cb) => {
-  const query = `${analyze ? 'EXPLAIN ANALYZE ' : ''}DELETE from users WHERE user_id = $1`;
+const deleteUserByID = async (user_id, analyze, cb) => {
+  const query = `${
+    analyze ? "EXPLAIN ANALYZE " : ""
+  }DELETE from users WHERE user_id = $1`;
   try {
     const res = await pool.query(query, [user_id]);
     cb(null, res);
   } catch (err) {
     cb(err, null);
   }
-}
+};
 
 // insert a photo
 const insertPhoto = async (document, analyze, cb) => {
-  const { restaurant_id, user_id, helpful_count, not_helpful_count, photo_url, caption, upload_date } = document;
-  const query = `${analyze ? 'EXPLAIN ANALYZE ' : ''}INSERT into photos(restaurant_id, user_id, helpful_count, not_helpful_count, photo_url, caption, upload_date) VALUES($1, $2, $3, $4, $5, $6, $7)`;
+  const {
+    restaurant_id,
+    user_id,
+    helpful_count,
+    not_helpful_count,
+    photo_url,
+    caption,
+    upload_date,
+  } = document;
+  const query = `${
+    analyze ? "EXPLAIN ANALYZE " : ""
+  }INSERT into photos(restaurant_id, user_id, helpful_count, not_helpful_count, photo_url, caption, upload_date) VALUES($1, $2, $3, $4, $5, $6, $7)`;
   try {
-    const res = await pool.query(query, [restaurant_id, user_id, helpful_count, not_helpful_count, photo_url, caption, upload_date]);
+    const res = await pool.query(query, [
+      restaurant_id,
+      user_id,
+      helpful_count,
+      not_helpful_count,
+      photo_url,
+      caption,
+      upload_date,
+    ]);
     cb(null, res);
   } catch (err) {
     cb(err, null);
   }
-}
+};
 
 // select photos by restaurant id
-const selectPhotoByID = async (restaurant_id, analyze, cb) => {
-  const query = `${analyze ? 'EXPLAIN ANALYZE ' : ''}SELECT * from photos WHERE restaurant_id = $1`;
+const selectPhotoByRestaurantID = async (restaurant_id, analyze, cb) => {
+  const query = `${
+    analyze ? "EXPLAIN ANALYZE " : ""
+  }SELECT * from photos WHERE restaurant_id = $1`;
   try {
     const res = await pool.query(query, [restaurant_id]);
     cb(null, res);
   } catch (err) {
     cb(err, null);
   }
-}
+};
 
 // delete a photo by photo id
-const deletePhotoByID = async(photo_id, analyze, cb) => {
-  const query = `${analyze ? 'EXPLAIN ANALYZE ' : ''}DELETE from photos WHERE photo_id = $1`;
+const deletePhotoByID = async (photo_id, analyze, cb) => {
+  const query = `${
+    analyze ? "EXPLAIN ANALYZE " : ""
+  }DELETE from photos WHERE photo_id = $1`;
   try {
     const res = await pool.query(query, [photo_id]);
     cb(null, res);
   } catch (err) {
     cb(err, null);
   }
-}
+};
 
 module.exports = {
   connectToClient,
--- server/index.js
+++ server/index.js
@@ -1,33 +1,39 @@
-require('newrelic');
-const express = require('express');
-const path = require('path');
-const db = require('./dbs/postgres-db');
+require("newrelic");
+const express = require("express");
+const path = require("path");
+const db = require("./dbs/postgres-db");
 
 const app = express();
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '/../public')));
+app.use(express.static(path.join(__dirname, "/../public")));
 
-// Restaurant API
-app.get('/api/restaurants/:id', (req, res) => {
+/**
+ * Select all restaurants matching restaurant ID
+ */
+app.get("/api/restaurants/:id", (req, res) => {
   const { id } = req.params;
   db.selectRestaurantByID(id, false, (err, data) => {
     res.status(200).send(data.rows[0]);
   });
 });
 
-// Gallery API
-app.get('/api/galleries/:id', (req, res) => {
+/**
+ * Select photos matching restaurant ID
+ */
+app.get("/api/galleries/:id", (req, res) => {
   const { id } = req.params;
   // console.log(id);
-  db.selectPhotoByID(id, false, (err, data) => {
-    res.status(200).send({gallery: data.rows});
+  db.selectPhotoByRestaurantID(id, false, (err, data) => {
+    res.status(200).send({ gallery: data.rows });
   });
 });
 
-// Photo API
-app.post('/api/photos/', (req, res) => {
+/**
+ * Post a photo
+ */
+app.post("/api/photos/", (req, res) => {
   const photo = req.body;
   db.insertPhoto(photo, false, (err, data) => {
     console.log(`${data.command}ed a photo!`);
@@ -35,15 +41,10 @@ app.post('/api/photos/', (req, res) => {
   });
 });
 
-app.patch('/api/photos/id', (req, res) => {
-  const photo = req.body;
-/*   db.updateOne(photo, false, (err, data) => {
-    console.log(`${data.command}ed a photo!`);
-    res.sendStatus(201);
-  }); */
-});
-
-app.delete('/api/photos/:id', (req, res) => {
+/**
+ * Delete a photo by photo id
+ */
+app.delete("/api/photos/:id", (req, res) => {
   const { id } = req.params;
   // console.log(id);
   db.deletePhotoByID(id, false, (err, data) => {
@@ -52,6 +53,27 @@ app.delete('/api/photos/:id', (req, res) => {
   });
 });
 
+/**
+ * Insert a user
+ */
+app.post("/api/users/", (req, res) => {
+  const user = req.body;
+  db.insertUser(user, false, (err, data) => {
+    console.log(`${data.command}ed a user!`);
+    res.sendStatus(201);
+  });
+});
+
+/**
+ * Get users matching user id
+ */
+app.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  db.selectUserByID(id, false, (err, data) => {
+    res.status(200).send({ users: data.rows });
+  });
+});
+
 app.listen(3001, () => {
-  console.log('listening on http://localhost:3001');
+  console.log("listening on http://localhost:3001");
 });
